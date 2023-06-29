6ae843e118960f7aee06fce1dfea335bb0cc9508
levando20194017 <levando194017@gmail.com>
2023-06-29 16:17:26 +0700

    update

--- server/index.js
+++ server/index.js
@@ -22,6 +22,8 @@ if (logRequests) {
 // Serve the static files from the React app
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 app.use(subDir, express.static(buildDir));
