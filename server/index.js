1959d0404e0bcde067d7bd5d44e6f036f3b9fe7e
levando20194017 <levando194017@gmail.com>
2023-06-29 16:22:43 +0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -21,9 +21,7 @@ if (logRequests) {
 
 // Serve the static files from the React app
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Origin', 'https://codecrush-hazel.vercel.app');
     next();
 });
 app.use(subDir, express.static(buildDir));
