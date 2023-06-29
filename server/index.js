efed07a00cd33036d03ebaad7c63f9936a708c0a
levando20194017 <levando194017@gmail.com>
2023-06-29 16:34:24 +0700

    update

--- server/index.js
+++ server/index.js
@@ -20,19 +20,6 @@ if (logRequests) {
 }
 
 // Serve the static files from the React app
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://codecrush-seven.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-app.options('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://codecrush-seven.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.status(200).send();
-});
 app.use(subDir, express.static(buildDir));
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
